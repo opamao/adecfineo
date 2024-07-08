3bad9067c6ab30ae2f2603d0c90f3c8eff9716f6
opamao <theodoreyapi@gmail.com>
2024-07-08 03:04:26 +0000

    menus

--- lib/src/features/menus/pages/menu_page.dart
+++ lib/src/features/menus/pages/menu_page.dart
@@ -1,4 +1,6 @@
+import 'package:adecfineo/src/themes/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class MenuPage extends StatefulWidget {
   const MenuPage({super.key});
@@ -10,8 +12,137 @@ class MenuPage extends StatefulWidget {
 class _MenuPageState extends State<MenuPage> {
   int currentPageIndex = 0;
 
+  final Widget _home = Container();
+  final Widget _compte = Container();
+  final Widget _transfert = Container();
+  final Widget _credit = Container();
+  final Widget _others = Container();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final drawerHeader = UserAccountsDrawerHeader(
+      accountName: const Text(
+        "YAPI N'GUESSAN KOUASSI THEODORE",
+      ),
+      accountEmail: const Text(
+        "+2250585831647",
+      ),
+      currentAccountPicture: ClipOval(
+        child: Image.asset("assets/images/user.jpeg"),
+      ),
+    );
+    final drawerItems = ListView(
+      children: [
+        drawerHeader,
+        ListTile(
+          title: const Text(
+            "Item1",
+          ),
+          leading: const Icon(Icons.favorite),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          title: const Text(
+            "item2",
+          ),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+    return Scaffold(
+      drawer: Drawer(
+        child: drawerItems,
+      ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: currentPageIndex == 0 ? appColor : appWhite,
+        leading: Padding(
+          padding: EdgeInsets.all(2.w),
+          child: Image.asset(
+            currentPageIndex == 0
+                ? "assets/images/logowhite.png"
+                : "assets/images/logoblue.png",
+          ),
+        ),
+        actions: [
+          Builder(builder: (context) {
+            return IconButton(
+              icon: Icon(
+                Icons.filter_list_outlined,
+                color: currentPageIndex == 0 ? appWhite : appColor,
+              ),
+              onPressed: () => Scaffold.of(context).openDrawer(),
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          }),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: appWhite,
+        surfaceTintColor: appWhite,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: <Widget>[
+          NavigationDestination(
+            icon: Icon(
+              Icons.home_outlined,
+              color: currentPageIndex == 0 ? appOrange : appBlack,
+            ),
+            label: "Accueil",
+          ),
+          NavigationDestination(
+            icon: Icon(
+              Icons.person_outlined,
+              color: currentPageIndex == 1 ? appOrange : appBlack,
+            ),
+            label: "Compte",
+          ),
+          NavigationDestination(
+            icon: Icon(
+              Icons.swap_horiz_outlined,
+              color: currentPageIndex == 2 ? appOrange : appBlack,
+            ),
+            label: "Transfert",
+          ),
+          NavigationDestination(
+            icon: Icon(
+              Icons.wallet_outlined,
+              color: currentPageIndex == 3 ? appOrange : appBlack,
+            ),
+            label: "Crédit",
+          ),
+          NavigationDestination(
+            icon: Icon(
+              Icons.widgets_outlined,
+              color: currentPageIndex == 4 ? appOrange : appBlack,
+            ),
+            label: "Autes",
+          ),
+        ],
+      ),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    if (currentPageIndex == 0) {
+      return _home;
+    } else if (currentPageIndex == 1) {
+      return _compte;
+    } else if (currentPageIndex == 2) {
+      return _transfert;
+    } else if (currentPageIndex == 3) {
+      return _credit;
+    } else {
+      return _others;
+    }
   }
 }
--- lib/src/themes/app_colors.dart
+++ lib/src/themes/app_colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 Color appColor = const Color(0xFF059CD8);
 Color appOrange = const Color(0xFFF9791C);
+Color appBlack = const Color(0xFF000000);
+Color appGrey = const Color(0xFFEEF0F1);
 Color appWhite = const Color(0xFFFFFFFF);
