5ecc9bee1401c362894ce39d93c91d732487ec24
opamao <theodoreyapi@gmail.com>
2024-07-08 02:01:05 +0000

    organisation des dossiers

---
+++ lib/src/themes/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color appColor = const Color(0xFF059CD8);
+Color appOrange = const Color(0xFFF9791C);
+Color appWhite = const Color(0xFFFFFFFF);
---
+++ lib/src/widgets/buttons/cancel_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+import '../../themes/themes.dart';
+
+
+class CancelButton extends StatelessWidget {
+  final String title;
+  final Function() onPressed;
+  final double? height;
+  final double? fontSize;
+
+  const CancelButton(
+      this.title, {super.key,
+        required this.onPressed,
+        this.height,
+        this.fontSize,
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: height ?? 12.w,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: appWhite,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(3.w),
+            side: BorderSide(color: appColor),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: fontSize ?? 12.sp,
+            color: appColor,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
